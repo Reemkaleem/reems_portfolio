5b894c9feda4946102046c6556f50eb598bb492d
Reemkaleem <reemk3103@gmail.com>
2026-02-01 01:02:13 +0530

    Remove all 3D elements on mobile for better performance

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
 import { BrowserRouter } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 const App = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 768px)");
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
   return (
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
@@ -17,7 +35,7 @@ const App = () => {
         <Feedbacks />
         <div className='relative z-0'>
           <Contact />
-          <StarsCanvas />
+          {!isMobile && <StarsCanvas />}
         </div>
       </div>
     </BrowserRouter>
